c83e162826ffec3f81a776c27aad5cd64afea553
Md Rabbi <mdfrabbi1975@gmail.com>
2025-06-30 02:12:16 -0400

    Add TabView

---
+++ Instagram/Core/TabBar/MainTabView.swift
@@ -0,0 +1,44 @@
+//
+//  MainTabView.swift
+//  Instagram
+//
+//  Created by Md Rabbi on 6/30/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .tint(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
