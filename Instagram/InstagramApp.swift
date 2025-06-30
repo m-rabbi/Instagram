85fd2b1cdc07bd83b7b68e9765a4d9fdc38d975b
Md Rabbi <mdfrabbi1975@gmail.com>
2025-06-30 01:36:42 -0400

    Initial Commit for Instagram Clone Project

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Md Rabbi on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
