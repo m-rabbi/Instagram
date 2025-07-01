8e677396c5f1a47e009ee08f3d6de80c58795c77
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-01 01:02:19 -0400

    Implement FeedView

--- Instagram/Core/Feed/FeedView.swift
+++ Instagram/Core/Feed/FeedView.swift
@@ -9,10 +9,27 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(0 ... 10, id: \.self) { index in
-                    FeedCell()
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { index in
+                        FeedCell()
+                    }
+                }
+            }
+            .padding(.top, 8)
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image("instagram-logo-png")
+                        .resizable()
+                        .frame(width: 100, height: 32)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
                 }
             }
         }
