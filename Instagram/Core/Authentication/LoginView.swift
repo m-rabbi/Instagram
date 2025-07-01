026ec14d182e6b26501cf1bd49fdf4b83bfed10a
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-01 03:11:24 -0400

    ui(auth): implement LoginView

---
+++ Instagram/Core/Authentication/LoginView.swift
@@ -0,0 +1,123 @@
+//
+//  LoginView.swift
+//  Instagram
+//
+//  Created by Md Rabbi on 7/1/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                // logo image
+                Image("instagram-logo-png")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 220, height: 100)
+                
+                // text fields
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .autocapitalization(.none)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6).opacity(0.8))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6).opacity(0.8))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+                
+                // forgot passowrd
+                Button {
+                    print("show forgot password")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                // log in button
+                Button {
+                    print("login")
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 360, height: 44)
+                        .background(Color(.systemBlue))
+                        .cornerRadius(8)
+
+                }
+                .padding(.vertical)
+                
+                // Divider
+                HStack {
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                    
+                    Text("OR")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                }
+                .foregroundStyle(.gray)
+                
+                // continue with facebook
+                HStack {
+                    Image("facebook-logo")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 24, height: 24)
+                    
+                    Text("Continue with Facebook")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(Color(.systemBlue))
+                }
+                .padding(.top, 8)
+                
+                Spacer()
+                
+                Divider()
+                
+                // footer
+                NavigationLink {
+                    Text("Sign Up")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+                .padding(.vertical, 16)
+
+
+                
+
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
