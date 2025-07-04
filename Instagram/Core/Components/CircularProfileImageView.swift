ea9de5e1541a62fefd710b11cbddde27344b50e8
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-04 03:20:21 -0400

    feat(profile): add CircularProfileImageView with Kingfisher for cached profile images

---
+++ Instagram/Core/Components/CircularProfileImageView.swift
@@ -0,0 +1,34 @@
+//
+//  CircularProfileImageView.swift
+//  Instagram
+//
+//  Created by Md Rabbi on 7/4/25.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct CircularProfileImageView: View {
+    let user: User
+    
+    var body: some View {
+        if let imageUrl = user.profileImageUrl {
+            KFImage(URL(string: imageUrl))
+                .resizable()
+                .scaledToFill()
+                .frame(width: 80, height: 80)
+                .clipShape(Circle())
+            
+        } else {
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: 80, height: 80)
+                .clipShape(Circle())
+            
+        }
+    }
+}
+
+#Preview {
+    CircularProfileImageView(user: User.MOCK_USERS[0])
+}
--- Instagram/Core/Profile/Views/ProfileHeaderView.swift
+++ Instagram/Core/Profile/Views/ProfileHeaderView.swift
@@ -15,11 +15,7 @@ struct ProfileHeaderView: View {
         VStack(spacing: 10) {
             // pic and stats
             HStack {
-                Image(user.profileImageUrl ?? "placeholder-image")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 80, height: 80)
-                    .clipShape(Circle())
+                CircularProfileImageView(user: user)
                 
                 Spacer()
                 
