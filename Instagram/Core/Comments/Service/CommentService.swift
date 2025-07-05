1ceed22d81b72f77bba3c8b1025ee35caed941db
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-05 10:17:12 -0400

    feat(comment): add CommentsViewModel with uploadComment function and CommentService for Firestore integration

---
+++ Instagram/Core/Comments/Service/CommentService.swift
@@ -0,0 +1,17 @@
+//
+//  CommentService.swift
+//  Instagram
+//
+//  Created by Md Rabbi on 7/5/25.
+//
+
+import FirebaseFirestore
+
+
+struct CommentService {
+    static func uploadComment(_ comment: Comment, postId: String) async throws {
+        guard let commentData = try? Firestore.Encoder().encode(comment) else { return }
+        
+        try await Firestore.firestore().collection("posts").document(postId).collection("post-comments").addDocument(data: commentData)
+    }
+}
---
+++ Instagram/Core/Comments/ViewModel/CommentsViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  CommentViweModel.swift
+//  Instagram
+//
+//  Created by Md Rabbi on 7/5/25.
+//
+
+import FirebaseAuth
+import FirebaseCore
+
+class CommentsViewModel: ObservableObject {
+    @Published var comments = [Comment]()
+    
+    private let post: Post
+    
+    init(post: Post){
+        self.post = post
+    }
+    
+    func uploadComment(commentText: String) async throws {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let comment = Comment(postOwnerUid: post.ownerUid, commentText: commentText, postId: post.id, timestamp: Timestamp(), commentOwnerUid: uid)
+        
+        try await CommentService.uploadComment(comment, postId: post.id)
+    }
+}
